2029cc8760873e84ad595e06a80552060f1abe30
aryafire1 <erthorsen876@gmail.com>
2024-09-11 14:04:33 -0400

    bro schmoovin

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public int speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+
+    void Movement() {
+        transform.Translate(new Vector3(Input.GetAxis("Horizontal"), 0, 0) * Time.deltaTime * speed);
+    }
+    
+}
